4f92ccd66aeada0db93f332e74adc799138e9aa5
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2025-03-25 00:37:26 +0600

    some css updates

--- src/app/store/product-details/[product_id]/page.jsx
+++ src/app/store/product-details/[product_id]/page.jsx
@@ -32,7 +32,8 @@ export default async function ProductDetails({ params }) {
               <li><strong>Category:</strong> {product.category}</li>
             </ul>
 
-            <button className='mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg'>
+            <button className='mt-6 px-6 py-3 bg-blue-500 text-white font-semibold 
+            rounded-lg cursor-pointer hover:bg-blue-600'>
               Add to Cart
             </button>
           </div>
--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -6,19 +6,21 @@ import Container from '../Container';
 export default function HeroSection() {
   return (
     <Container>
-      <section className={styles.hero}>
+      <section className={`${styles.hero} flex flex-col lg:flex-row items-center gap-8 lg:gap-0`}>
         {/* Text Section */}
-        <div className={`${styles.textSection} text-gray-700`}>
-          <h1 className='whitespace-nowrap'>
+        <div className={`${styles.textSection} text-gray-700 w-full lg:w-1/2 px-4 lg:px-0`}>
+          <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl whitespace-nowrap'>
             One stop solution <span className='text-pink-500'>E-Store</span></h1>
-          <p className='whitespace-nowrap'>
+          <p className='text-sm sm:text-base md:text-lg whitespace-nowrap'>
             Discover the latest headphones, earphones, mobiles, tablets etc.
           </p>
-          <p>Exclusive deals just for you!</p>
-          <button className={styles.ctaButton}>Shop Now</button>
+          <p className='text-sm sm:text-base md:text-lg'>Exclusive deals just for you!</p>
+          <button className={`${styles.ctaButton} mt-4`}>Shop Now</button>
         </div>
         {/* Image Slider Section */}
-        <Slider />
+        <div className='w-full lg:w-1/2'>
+          <Slider />
+        </div>
       </section>
     </Container>
   )
